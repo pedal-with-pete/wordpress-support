cc29bef54fb2a4b8c6be22c4ab87d86b51bee991
Uche Chilaka <uche@lar.city>
2022-12-08 02:36:42 -0500

    refactor: working logic to call e2e workflow

--- app/functions/e2e-test-runner/index.ts
+++ app/functions/e2e-test-runner/index.ts
@@ -1,6 +1,16 @@
 import { AzureFunction, Context } from "@azure/functions"
 import {request} from 'gaxios'
 
+function getDispatchEndpoint() {
+  const { 
+    GITHUB_REPO_OWNER: OWNER,
+    GITHUB_REPO: REPO_NAME,
+    GITHUB_WORKFLOW_IDENTIFIER: WORKFLOW_ID
+  } = process.env
+
+  return `https://api.github.com/repos/${OWNER}/${REPO_NAME}/actions/workflows/${WORKFLOW_ID}/dispatches`
+}
+
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
     var timeStamp = new Date().toISOString();
     
@@ -8,14 +18,17 @@ const timerTrigger: AzureFunction = async function (context: Context, myTimer: a
         context.log('Timer function is running late!');
     }
 
-    const { 
-      GITHUB_ACTION_PAT,
-      GITHUB_REPO_OWNER,
-      GITHUB_REPO,
-      GITHUB_WORKFLOW_IDENTIFIER
-    } = process.env
-
-    console.debug({ GITHUB_REPO, GITHUB_REPO_OWNER, GITHUB_WORKFLOW_IDENTIFIER })
+    const res = await request({
+      url: getDispatchEndpoint(),
+      method: 'POST',
+      headers: {
+        Accept: 'application/vnd.github+json',
+        Authorization: `Bearer ${process.env.GITHUB_ACTION_PAT}` 
+      },
+      data: { ref: 'main' }
+    })
+    
+    console.debug({ res })
 
     context.log('Timer trigger function ran!', timeStamp);   
 };
