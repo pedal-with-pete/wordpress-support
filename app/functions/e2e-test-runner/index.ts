b0ca0bce7175c3652714e1b7963c86f0c7a58514
Uche Chilaka <uche@lar.city>
2022-12-08 02:24:24 -0500

    refactor: (WIP) updating function script

--- app/functions/e2e-test-runner/index.ts
+++ app/functions/e2e-test-runner/index.ts
@@ -1,4 +1,5 @@
 import { AzureFunction, Context } from "@azure/functions"
+import {request} from 'gaxios'
 
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
     var timeStamp = new Date().toISOString();
@@ -6,6 +7,15 @@ const timerTrigger: AzureFunction = async function (context: Context, myTimer: a
     if (myTimer.isPastDue) {
         context.log('Timer function is running late!');
     }
+
+    const { 
+      GITHUB_ACTION_PAT,
+      GITHUB_REPO_OWNER,
+      GITHUB_REPO,
+      GITHUB_WORKFLOW_IDENTIFIER
+    } = process.env
+
+    console.debug({ GITHUB_REPO, GITHUB_REPO_OWNER, GITHUB_WORKFLOW_IDENTIFIER })
 
     context.log('Timer trigger function ran!', timeStamp);   
 };
