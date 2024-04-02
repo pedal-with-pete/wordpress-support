bc97513f0eef67f2128d6d825869aeee3976fe76
Uche Chilaka <uche@lar.city>
2024-04-02 09:15:14 -0400

    refactor: tweak global config timeouts

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
+  // See https://docs.cypress.io/guides/references/configuration#Timeouts
   defaultCommandTimeout: 10000,
+  requestTimeout: 12000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
