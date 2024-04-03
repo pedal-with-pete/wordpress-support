e5cf2e4e40286b81fb00355ae60f63a40129d985
Uche Chilaka <uche@lar.city>
2024-04-02 22:38:59 -0400

    refactor: making all payment specs adhoc (and skipped in CI)

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const { getVideosFolder, getExcludeSpecPattern } = require("./cypress/support/utils");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
@@ -7,11 +8,19 @@ module.exports = defineConfig({
   // See https://docs.cypress.io/guides/references/configuration#Timeouts
   defaultCommandTimeout: 10000,
   requestTimeout: 12000,
+  // See https://docs.cypress.io/guides/references/configuration#Viewport
+  viewportHeight: 900,
+  viewportWidth: 1440,
+  // See https://docs.cypress.io/guides/references/configuration#Videos
+  video: process.env.CI === "true" ? false : true,
+  videosFolder: getVideosFolder(),
+  videoCompression: 42,
   e2e: {
     specPattern: [
       "cypress/e2e/1-donate/*.cy.{js,jsx}",
       "cypress/e2e/2-volunteer/*.cy.{js,jsx}",
     ],
+    excludeSpecPattern: getExcludeSpecPattern(),
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,8 +1,25 @@
+const dayjs = require('dayjs')
+
 const getWebsiteURL = (uri = null) => {
   const baseURL = process.env.WEBSITE_URL || 'https://www.pedalwithpete.org'
   return [baseURL, uri].filter(part => part).join('/')
 }
 
+const getVideosFolder = () => {
+  if (!!process.env.CI) return 'cypress/videos'
+  return `cypress/videos/${dayjs().format('YYYY.MM')}`
+}
+
+const getExcludeSpecPattern = () => {
+  const excludeSpecPattern = [
+    (!!process.env.CI || !!process.env.CYPRESS_EXCLUDE_ADHOC_SPECS) && 'cypress/e2e/**/*{-,.}adhoc.cy.js'
+  ]
+
+  return excludeSpecPattern.filter(val => val)
+}
+
 module.exports = {
-  getWebsiteURL
+  getWebsiteURL,
+  getVideosFolder,
+  getExcludeSpecPattern
 }
